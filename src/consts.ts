9cd98aa62abe259e3fbdc52ae5af1dcdcf6fde83
Vladislav Tolomanenko <vlad.tolomanenko@sharp-dev.net>
2023-07-04 23:46:46 +0300

    init main page markup

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,33 @@
 
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const MAIN_PAGE_SLOTS = [
+  {
+    title: 'The Dog House™',
+    description: 'игровой автомат от разработчика Pragmatic Play. Слот посвящен собакам, отличается веселым дизайном и оригинальными возможностями специальных символов.',
+    imageSrc: 'images/dog-house.png',
+    width: 220,
+    height: 330
+  },
+  {
+    title: 'John Hunter and the Tomb of the Scarab Queen™',
+    description: 'Игровой автомат John Hunter and the Tomb of the Scarab Queen от разработчиков Pragmatic Play с тематикой приключений. Процент отдачи 96.50%, 25 линий выплат.',
+    imageSrc: 'images/hunter.png',
+    width: 220,
+    height: 330
+  },
+  {
+    title: 'Gates of Olympus™',
+    description: 'Врата в царство Зевса широко открыты для всех игроков в Gates of Olympus™, видеослоте 6×5 с 20 линиями выплат, где символы падают вниз, выплачивая их группами не менее 8.',
+    imageSrc: 'images/gates.png',
+    width: 220,
+    height: 330
+  },
+  {
+    title: 'Sweet Bonanza™',
+    description: 'Слот-игра от компании Pragmatic Play. Это игра с 6 барабанами, 10 линиями выплат и коэффициентом отдачи 96,51%. ',
+    imageSrc: 'images/sweet-bonanza.png',
+    width: 220,
+    height: 330
+  },
+]
